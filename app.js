02e6684f421e0946f9bfb5aa4ca710570ba0e7ea
star <502545703@qq.com>
2019-11-15 18:12:41 +0800

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const server = require('./lib/server');
-const client = require('./lib/client');
+const server_consume = require('./lib/server_consume');
+const client_send = require('./lib/client_send');
 const logger = require('./util/logger');
 const queueName = 'test';
 
@@ -7,16 +7,25 @@ const sleep = n => new Promise((resolve, reject) => setTimeout(resolve, n));
 
 async function test() {
   // 消费者：服务端监听并处理消息
-  server(queueName);
+  server_consume(queueName);
 
   // 客户端向消息队列发送消息
   let i = 1;
-  await sleep(1000);
-  client(queueName, `task${i}`);
+  client_send(queueName, `task${i++}`);
   await sleep(2000);
-  client(queueName, `task${i++}`);
+  client_send(queueName, `task${i++}`);
   await sleep(3000);
-  client(queueName, `task${i++}`);
+  client_send(queueName, `task${i++}`);
 }
 
 test().catch(console.error);
+
+process.on('uncaughtException', async err => {
+  await logger.error(err);
+  process.exit();
+});
+
+process.on('unhandledRejection', async (err, reason) => {
+  await logger.error(err, reason);
+  process.exit();
+});
